ca0c4ce3c36582b379995074a4acdf7f24f477b4
PuddingNight <scratmine@gmail.com>
2023-05-01 23:50:08 +0200

    adding lobby
    Toilets and lobby added, return paths added too

--- events.py
+++ events.py
@@ -149,6 +149,7 @@ def glowing_window():
    time.sleep(4)
    print("You can't quite make out what it is, but it's definitely something out of the ordinary.")
    time.sleep(4)
+
    while True:
       print("What do you want to do?")
       time.sleep(2)
@@ -204,6 +205,32 @@ def return_to_main_entrance():
    time.sleep(4)
    print("As you continue your search, you can't shake the feeling that you're being watched.")
    time.sleep(4)
+   print("After a long walk, you almost broke your ankle due to some unseen holes in the ground.")
+   time.sleep(4)
+   print("Nature had time to claim the outskirts of the hospital. You cannot continue any further.")
+
+   while True:
+      print("You have to chose wich entrance you wish to take.")
+      time.sleep(2)
+      print("1. Try to climb up the window that leads to the first floor")
+      time.sleep(2)
+      print("2. Investigate on the glowing window.")
+      time.sleep("2")
+      print("3. The main entrance might be a safer option.")
+      time.sleep("2")
+      choice = input("Enter the number of your choice: ")
+
+      if choice == "1":
+         climbing_through_the_window()
+         break
+      elif choice == "2":
+         glowing_window()
+         break
+      elif choice == "3":
+         going_through_main_entrance()
+         break
+      else:
+         print("Invalid choice. Please enter a valid number.")
 
 
 def climbing_through_the_window():
@@ -217,34 +244,76 @@ def climbing_through_the_window():
    time.sleep(4)
    in_the_administrative_offices()
 
-def in_the_administrative_offices():
-   pass
+
 
 
 def entering_the_toilets_from_window():
    print("You carefully enter the bathroom, keeping an eye out for any signs of danger.")
    time.sleep(4)
+   print("You can see a door half open that leads to what seems to be a big room.")
+   time.sleep(4)
+   print("The sound of the wind whistles calmly through throughout the building.")
+   time.sleep(4)
+   print("your eyes start to get used to darkness and you gather up the courage to find out what is this glow.")
+   time.sleep(6)
    print("As you get closer to the glowing object, you realize it's a small music box.")
    time.sleep(4)
    print("It's covered in strange symbols and glyphs, and it seems to be emanating a faint energy.")
    time.sleep(5)
    print("You notice a small worn latch that keeps the box shut.")
    time.sleep(4)
-   print("What do you want to do?")
-   time.sleep(2)
-   print("1. Open the box.")
-   time.sleep(2)
-   print("2. Leave the box alone and continue exploring the hospital.")
-   time.sleep(2)
-   choice = input("Enter the number of your choice: ")
-   if choice == "1":
-       opening_the_music_box()
-   elif choice == "2":
-       kjk
-   else:
-      print("Invalid choice. Please enter a valid number.")
-      time.sleep(2)
 
+   while True:
+      print("What do you want to do?")
+      time.sleep(2)
+      print("1. Open the box.")
+      time.sleep(2)
+      print("2. Leave the box alone and continue through the door.")
+      time.sleep(2)
+      print("3. Leave the box alone and go back outside from the window.")
+      choice = input("Enter the number of your choice: ")
+
+      if choice == "1":
+         opening_the_music_box()
+         break
+      elif choice == "2":
+         entering_Hospital_loby()
+         break
+      elif choice == "3":
+         print("You decide to hop back outside, you feel that something is not right.")
+         time.sleep(4)
+         continue_exploring_outside()
+         break
+      else:
+         print("Invalid choice. Please enter a valid number.")
+         time.sleep(2)
+
+def entering_Hospital_lobys():
+   
+   while True:
+      print("What do you want to do?")
+      time.sleep(2)
+      print("1. Go outside from the main Entrance.")
+      time.sleep(2)
+      print("2. Try to open the staff only door.")
+      time.sleep(2)
+      print("3. Take the stairs for the first floor.")
+      time.sleep(2)
+      print("4. Go to the toilets of the lobby.")
+      choice = input("Enter the number of your choice: ")
+
+      if choice == "1":
+         print("You decide to leave the lobby through the main door.")
+         time.sleep(2)
+
+
+def return_to_front_hospital():
+   pass
+
+
+
+def entering_the_toilets_from_door():
+   pass
 
 def opening_the_music_box():
    print("You cautiously open the music box, revealing its contents.")
@@ -270,5 +339,8 @@ def going_through_main_entrance():
 
 
 
-   print("You notice a door that leads to a dark room, going deeper in the hospital.")
-   time.sleep(4)
+   
+
+def in_the_administrative_offices():
+   pass
+
